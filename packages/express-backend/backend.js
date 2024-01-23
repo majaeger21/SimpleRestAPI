dc0d9c76a5030834d3e409ace2491856827f3f37
Meyli Jaeger <mjaege02@calpoly.edu>
2024-01-22 20:07:07 -0800

    backend2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,12 +3,41 @@ import express from "express";
 
 const app = express();          // create an instance of Express
 const port = 8000;              // a constant to represent the port number we'll use to listen to incoming HTTP requests
+const users = {
+    users_list: [
+      {
+        id: "xyz789",
+        name: "Charlie",
+        job: "Janitor"
+      },
+      {
+        id: "abc123",
+        name: "Mac",
+        job: "Bouncer"
+      },
+      {
+        id: "ppp222",
+        name: "Mac",
+        job: "Professor"
+      },
+      {
+        id: "yat999",
+        name: "Dee",
+        job: "Aspring actress"
+      },
+      {
+        id: "zap555",
+        name: "Dennis",
+        job: "Bartender"
+      }
+    ]
+  };
 
 app.use(express.json());        // we set up our express app to process incoming data in JSON format
 
-app.get("/", (req, res) => {    // '/' is the URL pattern that will map to this endpoint
+app.get("/users", (req, res) => {    // '/' is the URL pattern that will map to this endpoint
                                 // process the request and send a response to the client that called the REST API
-  res.send("Hello World!");
+  res.send(users);
 });
 
 app.listen(port, () => {
